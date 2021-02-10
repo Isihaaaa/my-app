6524b37ad2117cdc4cb6e56ea68ddb01944b3a91
Hajdu István <hajdu.istvan05@gmail.com>
2021-02-10 16:53:41 +0100

    Create folder structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserModule } from './user/user.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    UserModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserService } from './services/user.service';
+import { TestComponentComponent } from './components/test-component/test-component.component';
+
+@NgModule({
+  providers: [UserService],
+  declarations: [RegisterComponent, LoginComponent, TestComponentComponent],
+  imports: [CommonModule],
+  exports: [RegisterComponent, LoginComponent],
+})
+export class UserModule {}
